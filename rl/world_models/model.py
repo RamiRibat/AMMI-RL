8f7a30f4fccd12b8b7b6db86b8d2f365d8125a0f
RamiSketcher <ramisketcher@gmail.com>
2022-02-13 10:01:08 +0000

    Update

--- rl/world_models/model.py
+++ rl/world_models/model.py
@@ -84,23 +84,141 @@ class SimpleModel(pl.LightningModule):
         return predictions, mu, log_sigma, sigma, sigma_inv
 
 
-    def trainModel(self):
-        pass
+    def trainModel(self, env_buffer, batch_size, m):
+        # batch_data = env_buffer.get_recent_data(batch_size)
+        # Os = batch_data['observations']
+        # As = batch_data['actions']
+        # self.obs_norm.update(Os)
+        # self.act_norm.update(As)
+
+        self.m = m
+
+        # NT = self.config['Algorithm']['Learning']['epSteps']
+        # Ni = self.config['Algorithm']['Learning']['iEpochs']
+        M0 = self.config['Model']['Network']['mEpochs']
+        # BSize = env_buffer.size
+        mEpochs = M0#int(NT*Ni*M0/BSize)
+        # mEpochs = int(300 - (9e-4)*BSize)
+
+        batch_size = self.config['Model']['Network']['batch_size']
+        dropout = self.config['Model']['Network']['dropout']
+        env_buffer.device = 'cpu'
+
+        data = DataModule(env_buffer, batch_size)
+        if dropout != None: self.train()
+        ws_summ = None
+        # checkpoint_callback = ModelCheckpoint(save_last=None)
+        dyn_trainer = pl.Trainer(max_epochs=mEpochs,
+        						 gpus=1,
+        						 weights_summary=ws_summ,
+        						 checkpoint_callback=False,
+        						 logger=False,
+        						 # progress_bar_refresh_rate=0,
+        						 # log_save_interval=100,
+        						 # callbacks=[checkpoint_callback]
+        						 )
+        # print('SimpleModel Self: ', self)
+        dyn_trainer.fit(self, data)
+        if dropout != None: self.eval()
+
+        env_buffer.device = self.config['Experiment']['device']
+        # # dyn_trainer.save_checkpoint(f"/home/rami/AI/RL/myGitHub/FUSION/lightning_logs/DynModelsCkPts/model{m+1}")
+        return self.Jdyn, mEpochs
 
 
-    """ Pytorch Lightning """
+
+	### PyTorch Lightning ###
+	# add: dropouts, regulaizers
     def configure_optimizers(self):
-        pass
+        opt = 'th.optim.' + self.config['Model']['Network']['optimizer']
+        lr = self.config['Model']['Network']['lr']
+        optimizer = eval(opt)(self.parameters(), lr=lr)
+        return optimizer
 
-    
+	# def compute_model_loss(self, batch):
+	# 	device = self.config['Experiment']['device']
+	# 	O, A, R, O_next, D = batch
+	# 	D = th.as_tensor(D, dtype=th.bool).to(device)
+
+	# 	Prediction, mean, log_std, std, inv_std = self(O, A) # dyn_delta, reward
+	# 	mean_target = th.cat([O_next - O, R], dim=-1)
+
+
+	# 	Jmean = th.mean(th.mean(th.square(mean - mean_target) * inv_std, dim=-1), dim=-1) # batch loss
+	# 	Jstd = th.mean(th.mean(log_std, dim=-1), dim=-1)
+	# 	pass
+
     def training_step(self, batch, batch_idx):
-        pass
+        device = self.config['Experiment']['device']
+        O, A, R, O_next, D = batch
+        D = th.as_tensor(D, dtype=th.bool).to(device)
 
+        Prediction, mean, log_std, std, inv_std = self(O, A) # dyn_delta, reward
+        mean_target = th.cat([O_next - O, R], dim=-1)
+
+        # 2 Compute obj function
+        # Jmean = th.mean(th.square(mean - mean_target) * inv_std * ~D) # batch loss
+        # Jstd = th.mean(th.abs(log_std) * ~D)
+        # Jmodel = Jmean + Jstd
+        # Jmodel += 0.01 * th.norm(th.sum(self.max_log_std) - th.sum(self.min_log_std))
+        # Jl2 = self.compute_l2_loss()
+        # Jdyn = Jmodel + Jl2
+        # print('additional ', 0.01 * (th.sum(self.max_log_std) - th.sum(self.min_log_std)))
+        # dynAcc = accuracy(Prediction, Backup)
+
+        # 2 Compute obj function
+        # Jmean = th.mean(th.mean(th.square(mean - mean_target) * inv_std, dim=-1), dim=-1) # batch loss
+        # Jstd = th.mean(th.mean(log_std, dim=-1), dim=-1)
+        Jmean = th.mean(th.mean(th.square(mean - mean_target) * inv_std * ~D, dim=-1), dim=-1) # batch loss
+        Jstd = th.mean(th.mean(log_std * ~D, dim=-1), dim=-1)
+        Jl2 = self.compute_l2_loss()
+        Jdyn = Jmean + Jstd + Jl2
+        Jdyn += 0.01 * (th.sum(self.max_log_std) - th.sum(self.min_log_std))
+
+
+
+        # Jmean = th.mean(th.mean(th.square(mean - mean_target) * inv_std * ~D, dim=-1), dim=-1) # batch loss
+        # Jstd = th.mean(th.mean(log_std * ~D, dim=-1), dim=-1)
+        # Jmodel =
+        # Jl2 = self.compute_l2_loss()
+        # Jdyn = Jmodel + Jl2
+        # Jdyn += 0.01 * (th.sum(self.max_log_std) - th.sum(self.min_log_std))
+
+
+        self.log(f'Model {self.m+1}, Loss', Jdyn.item(), prog_bar=True)
+        # self.log('Acc', dynAcc, prog_bar=True)
+        # print(f'Jdyn (simple): {Jdyn}')
+        self.Jdyn = Jdyn.item() # We no longer need it; bc it's auto optimized
+
+        # dynAcc = accuracy(Prediction, Backup)
+        # pbar = {'train_acc': dynAcc}
+        # return {'loss': Jdyn, 'progress_bar': pbar} # Lightning will auto detach it
+        # return {'loss': Jdyn}
+
+        return Jdyn
+
+	# def validation_step(self, batch, batch_idx):
+	# 	# In validation loop, we generally don't want to put a metrics for
+	# 	# every batch (all batches are independent), plot metric for the whole validation set
+	# 	results = self.training_step(batch, batch_idx)
+	# 	results['progress_bar']['val_acc'] = results['progress_bar']['train_acc']
+	# 	del results['progress_bar']['train_acc']
+	# 	return results
 
     def get_progress_bar_dict(self):
-        pass
+        # don't show the version number
+        items = super().get_progress_bar_dict()
+        items.pop("loss", None)
+        return items
 
 
+    # def compute_l2_loss(self, l2_loss_coefs: Union[float, List[float]]):
     def compute_l2_loss(self):
-        pass
-
+        l2_loss_coefs = [0.000025, 0.00005, 0.000075, 0.000075, 0.0001, 0.0001]
+        weight_norms = []
+        for name, weight in self.named_parameters():
+        	if "weight" in name:
+        		weight_norms.append(weight.norm(2))
+        weight_norms = T.stack(weight_norms, dim=0)
+        weight_decay = (T.tensor(l2_loss_coefs, device=weight_norms.device) * weight_norms).sum()
+        return weight_decay
--- rl/world_models/world_model.py
+++ rl/world_models/world_model.py
@@ -1,11 +1,22 @@
 # TODO: world_model
 import random
+from typing import Type, List, Tuple
+
 import numpy as np
 import torch as T
 from torch.distributions.normal import Normal
 nn = T.nn
 
-from .mlp import MLPNet
+import pytorch_lightning as pl
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+
+## Fusion
+from rl.world_models.simple import SimpleModel#, BayesianModel
+# from fusion.data.buffer.norm import RunningNormalizer
+
+# from rl.networks.mlp import MLPNet
 
 
 LOG_SIGMA_MAX = 2
@@ -13,6 +24,146 @@ LOG_SIGMA_MIN = -20
 
 
 
-class WorldModel(nn.Module):
-    def __init__(self) -> None:
-        pass
+class WorldModel(LightningModule):
+
+	def __init__(self, obs_dim, act_dim, rew_dim, config):
+		print('Initialize World Model!')
+        super(WorldModel, self).__init__() # To automatically use 'def forward'
+		# Set a random seed, se whenever we call crtics they will be consistent
+		seed = config['experiment']['seed']
+		# random.seed(seed)###########
+        if seed: np.random.seed(seed), T.manual_seed(seed)
+
+		device = self._device_ = config['experiment']['device']
+
+		self.models = []
+
+		if config['world_model']['type'] == 'P':
+			self.models = SimpleModel(obs_dim,
+									 act_dim,
+									 rew_dim,
+									 config
+									 ).to(device)
+		elif config['world_model']['type'] == 'PE':
+			M = config['world_model']['num_ensembles']
+			for m in range(M):
+				model = SimpleModel(obs_dim,
+										act_dim,
+										rew_dim,
+										config
+										).to(device)
+				self.models.append(model)
+		# elif config['world_model']['type'] == 'BE':
+		# 	M = config['world_model']['Ensembles']
+		# 	for m in range(M):
+		# 		dyn_model = BayesianModel(obs_dim,
+		# 								act_dim,
+		# 								rew_dim,
+		# 								config
+		# 								).to(device)
+		# 		self.dyn_models.append(dyn_model)
+
+		self.config = config
+		# print(self.models)
+
+
+	def sample(self, obs, act, determenistic, sample_type):
+		num_elites = self.config['world_model']['num_elites']
+
+		if num_elites == 1:
+			prediction, mean, log_std, std, inv_std = self.dyn_models(obs, act, determenistic)
+		else:
+			dyn_models_elites = [ self.dyn_models[i] for i in self.inx_elites ]
+			dyn_models_preds = [dyn_model(obs, act, determenistic) for dyn_model in dyn_models_elites]
+
+			if sample_type == 'Average':
+				prediction = [dyn_models_preds[i][0] for i in range(len(dyn_models_preds))]
+				mean = [dyn_models_preds[i][1] for i in range(len(dyn_models_preds))]
+				log_std = [dyn_models_preds[i][2] for i in range(len(dyn_models_preds))]
+				std = [dyn_models_preds[i][3] for i in range(len(dyn_models_preds))]
+				inv_std = [dyn_models_preds[i][4] for i in range(len(dyn_models_preds))]
+
+				prediction = sum(prediction) / num_elites
+				mean = sum(mean) / num_elites
+				std = sum(std) / num_elites
+
+			elif sample_type == 'Random':
+				indx = random.randint(1, num_elites)
+				prediction, mean, log_std, std, inv_std = dyn_models_preds[indx-1]
+			elif sample_type == 'All':
+				prediction = [dyn_models_preds[i][0] for i in range(len(dyn_models_preds))]
+				mean = [dyn_models_preds[i][1] for i in range(len(dyn_models_preds))]
+				log_std = [dyn_models_preds[i][2] for i in range(len(dyn_models_preds))]
+				std = [dyn_models_preds[i][3] for i in range(len(dyn_models_preds))]
+				inv_std = [dyn_models_preds[i][4] for i in range(len(dyn_models_preds))]
+			elif type(sample_type) == int:
+				prediction = dyn_models_preds[sample_type][0]
+				mean = dyn_models_preds[sample_type][1]
+				log_std = dyn_models_preds[sample_type][2]
+				std = dyn_models_preds[sample_type][3]
+				inv_std = dyn_models_preds[sample_type][4]
+
+		return prediction, mean, log_std, std, inv_std
+
+
+	def forward(self, obs, act, determenistic=False, sample_type='Average'):
+		normed_obs = obs#self.obs_norm(obs)
+		normed_act = act#self.act_norm(act)
+
+		M = self.config['world_model']['num_ensembles']
+		modelType = self.config['world_model']['type']
+		device = self._device_  # self.config['experiment']['device']
+
+		if modelType == 'P':
+			prediction, mean, log_std, std, inv_std = self.sample(normed_obs, normed_act, determenistic, sample_type)
+		elif modelType == 'PE':
+			prediction, mean, log_std, std, inv_std = self.sample(normed_obs, normed_act, determenistic, sample_type)
+
+		obs_next = prediction[:,:-1] + obs
+		rew = prediction[:,-1]
+		# print('xx:', th.cat([obs, th.zeros(obs.shape[0], 1).to(device)], dim=1).shape)
+		mean = mean + th.cat([obs, th.zeros(obs.shape[0], 1).to(device)], dim=1)
+
+		# print(f'Models: Mean {mean}, STD {std}')
+		return obs_next, rew, mean, std
+
+
+	### PyTorch Lightning ###
+	def train_WM(self, env_buffer, batch_size):
+        device = self._device_
+
+		M = self.config['world_model']['num_ensembles']
+		model_type = self.config['world_model']['type']
+		num_elites = self.config['world_model']['num_elites']
+
+        trainer = Trainer(max_epochs=n_epochs,
+                          # gpus=1,
+                          enable_model_summary=False,
+                          checkpoint_callback=False,
+                          progress_bar_refresh_rate=20,
+                          # log_save_interval=100,
+                          # logger=self.pl_logger,
+                          # callbacks=[checkpoint_callback],
+                           )
+
+		if modelType == 'P':
+			# Jdyn, mEpochs = self.dyn_models.trainModel(env_buffer, batch_size, 0)
+			# self.dyn_models.to(device) # bc pl-training detatchs models
+			# Jwm = Jdyns
+		elif modelType == 'PE':
+			J = []
+			for m in range(M):
+				# Jm, mEpochs = self.models[m].train_Model(env_buffer, batch_size, m)
+				Jm, mEpochs = self.models[m].train_Model(trainer, data_module, m)
+				# print(f'modle {m}: Jdyn = {Jdyn}')
+				J.append(Jm)
+				self.models[m].to(device) # bc pl-training detatchs models
+			inx_model = np.argsort(J)
+			self.inx_elites = inx_model[:num_elites]
+			# print(f'Jdyns = {Jdyns}')
+			Jwm = sum(J) / M
+
+		print('Jwm: ', round(Jwm, 5))
+		print('Elite Models: ', self.inx_elites)
+
+		return Jwm, mEpochs
