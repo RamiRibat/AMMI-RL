1bc6324cdeb8c26e4c9fc818489405ed859f207a
RamiSketcher <ramisketcher@gmail.com>
2022-02-13 10:40:27 +0000

    Add

---
+++ rl/data/dataset.py
@@ -0,0 +1,66 @@
+# import random
+# import numpy as np
+import torch as T
+from torch.utils.data import random_split, DataLoader
+from torch.utils.data.dataset import IterableDataset
+
+from pytorch_lightning import LightningDataModule
+
+
+class RLDataset(IterableDataset): # Take env_buffer, return all samples in a Dataset
+
+    def __init__(self, env_buffer):
+    	self.buffer = env_buffer
+
+    def __iter__(self) -> Tuple:
+    	data = self.buffer.return_all()
+
+    	Os = data['observations']
+    	As = data['actions']
+    	Rs = data['rewards']
+    	Os_next = data['observations_next']
+    	Ds = data['terminals']
+
+    	for i in range(len(Ds)):
+    		yield Os[i], As[i], Rs[i], Os_next[i], Ds[i]
+
+    def __len__(self):
+    	return self.buffer.size
+
+
+class RLDataModule(LightningDataModule):
+
+    def __init__(self, data_buffer, configs):
+        super(RLDataModule, self).__init__()
+        print('init RLDataModule!')
+        self.data_buffer = data_buffer
+        self.configs = configs
+
+    def setup(self, step):
+        val_ratio = self.configs['model_val_ratio']
+        rl_dataset = RLDataset(self.data_buffer) # Take env_buffer & sample all data
+
+        if stage == "fit" or stage is None:
+            val = int(val_ratio*len(rl_dataset))
+            train = len(rl_dataset) - val
+            self.train_set, self.val_set = random_split(rl_dataset, [train, val])
+
+    def train_dataloader(self):
+        batch_size = self.configs['model_batch_size']
+        train_loader = DataLoader(dataset=self.train_set,
+        						  batch_size=batch_size,
+            					  shuffle=False,
+        						  num_workers=4, # Calls X RLDataset.__iter__() times
+        						  # pin_memory=True
+        						  )
+        return train_loader
+
+    def val_dataloader(self):
+        batch_size = self.configs['model_batch_size']
+        val_loader = DataLoader(dataset=self.val_set,
+                                batch_size=batch_size,
+                                shuffle=False,
+                                num_workers=4, # Calls X RLDataset.__iter__() times
+                                # pin_memory=True
+        						  )
+        return val_loader
