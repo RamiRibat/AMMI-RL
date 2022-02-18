d70bf168b351f47145e74e1c7f65926a0f4299cc
RamiSketcher <ramisketcher@gmail.com>
2022-02-18 06:26:50 +0000

    Update VF

--- rl/value_functions/q_function.py
+++ rl/value_functions/q_function.py
@@ -6,14 +6,36 @@ nn = T.nn
 from rl.networks.mlp import MLPNet
 
 
+class QFunction(nn.Module):
+    """
+    Q-Function
+    """
+    def __init__(self, obs_dim, act_dim, net_configs, seed):
+        # print('init QFunction!')
+        # if seed: random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
+
+        optimizer = 'T.optim.' + net_configs['optimizer']
+        lr = net_configs['lr']
+
+        super().__init__() # To automatically use forward
+
+        self.q1 = MLPNet(obs_dim + act_dim, 1, net_configs)
+
+        self.optimizer = eval(optimizer)(self.parameters(), lr)
+
+
+    def forward(self, o, a):
+        q_inputs = T.cat([o, a], dim=-1)
+        return self.q1(q_inputs)
+
+
 class SoftQFunction(nn.Module):
     """
     Soft Q-Function
     """
     def __init__(self, obs_dim, act_dim, net_configs, seed):
-        # print('init QFunction!')
-        if seed:
-            random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
+        # print('init Soft QFunction!')
+        # if seed: random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
 
         optimizer = 'T.optim.' + net_configs['optimizer']
         lr = net_configs['lr']
