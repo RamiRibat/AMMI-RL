078af195459c9aa3d758b7ebaccd2bb509b9a0dc
RamiSketcher <ramisketcher@gmail.com>
2022-02-18 06:25:40 +0000

    Update setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         'matplotlib==3.4.3',
         'numpy==1.21.2',
         'torch==1.9.0',
-        'wandb==0.12.2',
+        'wandb>=0.12.10',
         'gym==0.20.0',
         'mujoco-py==2.0.2.13'
     ],
