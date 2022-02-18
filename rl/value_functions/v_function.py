401f5e0359c00de82091295970ce5f7a9a037010
RamiSketcher <ramisketcher@gmail.com>
2022-02-18 06:24:47 +0000

    Add VF

---
+++ rl/value_functions/v_function.py
@@ -0,0 +1,31 @@
+import random
+import numpy as np
+import torch as T
+nn = T.nn
+
+from rl.networks.mlp import MLPNet
+
+
+class VFunction(nn.Module):
+    """
+    V-Function
+    """
+    def __init__(self, obs_dim, act_dim, net_configs, seed):
+        # print('init QFunction!')
+        # if seed: random.seed(seed), np.random.seed(seed), T.manual_seed(seed)
+
+        optimizer = 'T.optim.' + net_configs['optimizer']
+        lr = net_configs['lr']
+
+        super().__init__() # To automatically use forward
+
+        self.q1 = MLPNet(obs_dim + act_dim, 1, net_configs)
+        self.q2 = MLPNet(obs_dim + act_dim, 1, net_configs)
+        self.Qs = [self.q1, self.q2]
+
+        self.optimizer = eval(optimizer)(self.parameters(), lr)
+
+
+    def forward(self, o, a):
+        q_inputs = T.cat([o, a], dim=-1)
+        return tuple(Q(q_inputs) for Q in self.Qs)
