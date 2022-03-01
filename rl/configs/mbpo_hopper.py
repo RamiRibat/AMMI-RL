3e9b80179911c9e1856bd586d9ded2bb2e17b417
RamiSketcher <ramisketcher@gmail.com>
2022-03-01 08:19:00 +0000

    Update

--- rl/configs/mbpo_hopper.py
+++ rl/configs/mbpo_hopper.py
@@ -131,7 +131,7 @@ configurations = {
         'device': "cpu",
         # 'device': "cuda:0",
         'WandB': True,
-        'WandB': False,
+        # 'WandB': False,
         'print_logs': True,
     }
 }
