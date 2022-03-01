87a5f4f57b89d02553d48fdd65612e760eb8c96a
RamiSketcher <ramisketcher@gmail.com>
2022-03-01 02:41:59 +0000

    Update

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setup(
         'numpy>=1.21.2',
         'torch>=1.9.0',
         'wandb>=0.12.10',
-        'gym>=0.20.0',
+        # 'gym>=0.20.0',
         # 'mujoco-py==2.0.2.13'
-        'mujoco-py<2.1,>=2.0'
+        # 'mujoco-py<2.1,>=2.0'
     ],
     description="RL-AMMI tools for combining deep RL algorithms.",
     authors="AAMI: MohamedElfatih Salah, Rami Ahmed*, Ruba Mutasim, Wafaa Mohammed",
