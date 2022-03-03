513969133c57e61f31a3d9c3d68873cd29b53d90
RamiSketcher <ramisketcher@gmail.com>
2022-03-03 08:52:20 +0000

    Update

--- rl/algorithms/mbrl/mbpo.py
+++ rl/algorithms/mbrl/mbpo.py
@@ -327,7 +327,7 @@ def main(exp_prefix, config, seed, device, wb):
     wm_epochs = configs['algorithm']['learning']['grad_WM_steps']
     DE = configs['world_model']['num_ensembles']
 
-    group_name = f"{env_name}-{alg_name}-DE{DE}-F"
+    group_name = f"{env_name}-{alg_name}-DE{DE}-G"
     exp_prefix = f"seed:{seed}"
 
     if wb:
--- rl/configs/mbpo_hopper.py
+++ rl/configs/mbpo_hopper.py
@@ -21,7 +21,7 @@ configurations = {
 
             'env_steps' : 1, # E: interact E times then train
             'grad_WM_steps': 10, # G: ac grad
-            'grad_SAC_steps': 40, #20, # ACG: ac grad, 40
+            'grad_SAC_steps': 20, #20, # ACG: ac grad, 40
 
             'policy_update_interval': 1,
             'alpha_update_interval': 1,
@@ -78,7 +78,7 @@ configurations = {
         'automatic_entropy': False, # trainer_kwargs
         'target_entropy': "auto",
         'network': {
-            'arch': [512, 512], #@#
+            'arch': [256, 256], #@#
             'init_weights': 3e-3,
             'init_biases': 0,
             'activation': 'ReLU',
@@ -98,7 +98,7 @@ configurations = {
         'gamma': 0.99,
         'tau': 5e-3,
         'network': {
-            'arch': [512, 512], #@#
+            'arch': [256, 256], #@#
             'init_weights': 3e-3,
             'init_biases': 0,
             'activation': 'ReLU',
@@ -118,7 +118,7 @@ configurations = {
         'optimize_memory_usage': False,
         'buffer_size': int(5e5),
         'model_buffer_size': int(1e7),
-        'real_ratio': 0.0,
+        'real_ratio': 0.05,
         'model_val_ratio': 0.2,
         'rollout_batch_size': 400,
         'model_batch_size': 256,
