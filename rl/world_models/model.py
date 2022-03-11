7d99c60aa6c2bda68966b57ae80f235c3cc73637
RamiSketcher <ramisketcher@gmail.com>
2022-03-11 04:56:05 +0000

    Update

--- rl/world_models/model.py
+++ rl/world_models/model.py
@@ -206,7 +206,7 @@ class DynamicsModel(LightningModule):
                           # log_every_n_steps=2,
                           # accelerator=device, devices='auto',
                           # gpus=[eval(device[-1])] if device[:-2]=='cuda' else 0,
-                          gpus=2, strategy='ddp',
+                          gpus=2, strategy='dp',
                           enable_model_summary=False,
                           enable_checkpointing=False,
                           progress_bar_refresh_rate=20,
