d3337bcf758af4118bb56fe833e50d8b8a81deba
RamiSketcher <ramisketcher@gmail.com>
2022-03-11 03:27:08 +0000

    Update

--- rl/control/policy.py
+++ rl/control/policy.py
@@ -47,19 +47,18 @@ class StochasticPolicy(nn.Module):
 
 		self.apply(init_weights_)
 
+		self.to(device)
 
 
 		# self.obs_bias   = np.zeros(obs_dim)
 		# self.obs_scale  = np.ones(obs_dim)
 
-		self.obs_bias   = T.zeros(obs_dim)#.to(device)
-		self.obs_scale  = T.ones(obs_dim)#.to(device)
+		self.obs_bias   = T.zeros(obs_dim).to(device)
+		self.obs_scale  = T.ones(obs_dim).to(device)
 
 		self.act_dim = act_dim
-		self.act_bias =  T.FloatTensor( (act_up_lim + act_low_lim) / 2.0 )#.to(device)
-		self.act_scale = T.FloatTensor( (act_up_lim - act_low_lim) / 2.0 )#.to(device)
-
-		self.to(device)
+		self.act_bias =  T.FloatTensor( (act_up_lim + act_low_lim) / 2.0 ).to(device)
+		self.act_scale = T.FloatTensor( (act_up_lim - act_low_lim) / 2.0 ).to(device)
 
 		# Define optimizer
 		self.optimizer = eval(optimizer)(self.parameters(), lr)
