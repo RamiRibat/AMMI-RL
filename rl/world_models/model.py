eb5c3c041e3b6f42495833f39fc6eee772534a6b
RamiSketcher <ramisketcher@gmail.com>
2022-03-11 03:43:21 +0000

    Update

--- rl/world_models/model.py
+++ rl/world_models/model.py
@@ -153,8 +153,9 @@ class DynamicsModel(LightningModule):
 
 
     def forward(self, o, a, deterministic= False):
-        print('self.obs_bias: ', self.obs_bias)
-        print('o: ', o)
+        print('\nself.obs_bias: ', self.obs_bias)
+        print('self.device: ', self.device)
+        # print('o: ', o)
         normed_o = (o - self.obs_bias)/(self.obs_scale + epsilon)
         normed_a = (a - self.act_bias)/(self.act_scale + epsilon)
 
