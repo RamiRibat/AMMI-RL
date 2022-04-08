3cb99db616bd7b2dc588ab64da616da1e5749696
RamiSketcher <ramisketcher@gmail.com>
2022-04-08 08:27:44 +0300

    Update

--- rl/data/buffer.py
+++ rl/data/buffer.py
@@ -7,7 +7,7 @@ Inspired by:
 """
 
 import random
-import scipy.signal
+# import scipy.signal
 import numpy as np
 import torch as T
 # from torch.utils.data import random_split, DataLoader
@@ -22,20 +22,20 @@ def combined_shape(length, shape=None):
     return (length, shape) if np.isscalar(shape) else (length, *shape)
 
 
-def discount_cumsum(x, discount): # source: https://github.com/openai/spinningup/
-    """
-    magic from rllab for computing discounted cumulative sums of vectors.
-    input:
-        vector x,
-        [x0,
-         x1,
-         x2]
-    output:
-        [x0 + discount * x1 + discount^2 * x2,
-         x1 + discount * x2,
-         x2]
-    """
-    return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
+# def discount_cumsum(x, discount): # source: https://github.com/openai/spinningup/
+#     """
+#     magic from rllab for computing discounted cumulative sums of vectors.
+#     input:
+#         vector x,
+#         [x0,
+#          x1,
+#          x2]
+#     output:
+#         [x0 + discount * x1 + discount^2 * x2,
+#          x1 + discount * x2,
+#          x2]
+#     """
+#     return scipy.signal.lfilter([1], [1, float(-discount)], x[::-1], axis=0)[::-1]
 
 
 
