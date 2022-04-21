cd0700c93db5a28ef503c01a43d0b4ddd6a6ec4c
RamiSketcher <ramisketcher@gmail.com>
2022-04-21 14:26:06 +0300

    Update data

--- rl/data/buffer.py
+++ rl/data/buffer.py
@@ -194,7 +194,7 @@ class TrajBuffer:
 
 
 
-class ReplayBufferPT:
+class ReplayBuffer:
     """
     FIFO Replay buffer for off-policy data:
         __init__: initialize: empty matrices for storing traj's, poniter, size, max_size
@@ -352,7 +352,7 @@ class ReplayBufferPT:
 
 
 
-class ReplayBuffer:
+class ReplayBufferNP:
     """
     FIFO Replay buffer for off-policy data:
         __init__: initialize: empty matrices for storing traj's, poniter, size, max_size
