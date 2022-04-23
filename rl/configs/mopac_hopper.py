6532bffd3122b6ea9bb8bd731737e02f75ecad9c
RamiSketcher <ramisketcher@gmail.com>
2022-04-23 03:52:41 +0300

    Update cfgs

--- rl/configs/mopac_hopper.py
+++ rl/configs/mopac_hopper.py
@@ -11,23 +11,24 @@ configurations = {
         },
 
     'algorithm': {
-        'name': 'MBPPO',
+        'name': 'MoPAC',
         'model-based': True,
-        'on-policy': True,
+        'on-policy': False,
+        'MPC': True,
         'learning': {
             'epochs': 500, # N epochs
             'epoch_steps': 1000, # NT steps/epoch
             'init_epochs': 5, # Ni epochs = 5000 exploration steps
-            'expl_epochs': 10, # Nx epochs
-            'real_epochs': 0, # Nr epochs
+            'expl_epochs': 0, # Nx epochs
 
             'env_steps' : 1, # E: interact E times then train
-            'grad_WM_steps': 50, # G: ac grad
-            'grad_SAC_steps': 20, # ACG: ac grad, 40
+            'grad_WM_steps': 0, # G: ac grad
+            'grad_SAC_steps': 20, #20, # ACG: ac grad, 40
 
             'policy_update_interval': 1,
             'alpha_update_interval': 1,
             'target_update_interval': 1,
+
 
             'n_episodes_rollout': -1,
 
@@ -52,21 +53,44 @@ configurations = {
         'num_elites': 5, # 5
         'sample_type': 'Random',
         'learn_reward': True,
-        'model_train_freq': 250,
+        'learn_log_sigma_limits': False,
+        'model_train_freq': 250,#250, # Mf
         'model_retain_epochs': 1,
-        'rollout_schedule': [20, 100, 1, 15],
+        'rollout_schedule': [20, 150, 1, 15],
         'network': {
-            'arch': [200,200,200,200], #@#
+            'arch': [200, 200, 200, 200], #@#
+            'init_weights': 3e-3,
+            'init_biases': 0,
+            'activation': 'SiLU',
+            'output_activation': 'nn.Identity',
+            'optimizer': "Adam", #@#
+            'lr': 1e-3, #@#
+            'wd': 1e-5,
+            'eps': 1e-8,
+            'dropout': None,
+            'batch_size': 256,
+            # 'device': "auto",
+        }
+    },
+
+    'traj_optz': {
+        'type': 'MPC',
+        'action_noise': None, # Optional
+        'alpha': 0.2, # Temprature/Entropy #@#
+        'automatic_entropy': False, # trainer_kwargs
+        'target_entropy': "auto",
+        'network': {
+            'arch': [256, 256], #@#
             'init_weights': 3e-3,
             'init_biases': 0,
             'activation': 'ReLU',
             'output_activation': 'nn.Identity',
             'optimizer': "Adam", #@#
-            'lr': 1e-3, #@#
+            'lr': 3e-4, #@#
             'wd': 1e-5,
             'dropout': None,
-            'batch_size': 250,
-            'device': "auto",
+            'batch_size': 256,
+            # 'device': "auto",
         }
     },
 
@@ -77,7 +101,7 @@ configurations = {
         'automatic_entropy': False, # trainer_kwargs
         'target_entropy': "auto",
         'network': {
-            'arch': [256,256], #@#
+            'arch': [256, 256], #@#
             'init_weights': 3e-3,
             'init_biases': 0,
             'activation': 'ReLU',
@@ -87,7 +111,7 @@ configurations = {
             'wd': 1e-5,
             'dropout': None,
             'batch_size': 256,
-            'device': "auto",
+            # 'device': "auto",
         }
     },
 
@@ -97,7 +121,7 @@ configurations = {
         'gamma': 0.99,
         'tau': 5e-3,
         'network': {
-            'arch': [256,256], #@#
+            'arch': [256, 256], #@#
             'init_weights': 3e-3,
             'init_biases': 0,
             'activation': 'ReLU',
@@ -107,7 +131,7 @@ configurations = {
             'wd': 1e-5,
             'dropout': None,
             'batch_size': 256,
-            'device': "auto",
+            # 'device': "auto",
         }
     },
 
@@ -118,11 +142,12 @@ configurations = {
         'buffer_size': int(5e5),
         'model_buffer_size': int(1e7),
         'real_ratio': 0.05,
+        # 'real_ratio': 0.0,
         'model_val_ratio': 0.2,
-        'rollout_batch_size': 400,
+        'rollout_batch_size': 100000,
         'model_batch_size': 256,
         'batch_size': 256,
-        'device': "auto",
+        # 'device': "auto",
     },
 
 
