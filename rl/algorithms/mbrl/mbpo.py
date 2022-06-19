36b661fe5f36f82106ff54aea6b3e759735c94cc
RamiSketcher <ramisketcher@gmail.com>
2022-06-20 00:57:11 +0200

    Update algs

--- rl/algorithms/mbrl/mbpo.py
+++ rl/algorithms/mbrl/mbpo.py
@@ -548,7 +548,7 @@ def main(exp_prefix, config, seed, device, wb):
     wm_epochs = configs['algorithm']['learning']['grad_WM_steps']
     DE = configs['world_model']['num_ensembles']
 
-    group_name = f"{env_name}-{alg_name}-Mac-A"
+    group_name = f"{env_name}-{alg_name}-Mac-B"
     # group_name = f"{env_name}-{alg_name}-GCP-A"
     exp_prefix = f"seed:{seed}"
 
