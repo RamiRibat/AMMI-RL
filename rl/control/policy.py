8264934aeb8781faa473bbc917d291bff57e6409
RamiSketcher <ramisketcher@gmail.com>
2022-06-22 00:00:57 +0200

    Update ctrl

--- rl/control/policy.py
+++ rl/control/policy.py
@@ -42,24 +42,7 @@ class PPOPolicy(nn.Module):
 		net_arch = net_configs['arch']
 		optimizer = 'T.optim.' + net_configs['optimizer']
 		lr = net_configs['lr']
-		# hid = 64
-		hid = 256 # PPO-G
-		# hid = 128 # PPO-H
-
-		# My suggestions:
-		# self.mean = MLPNet(obs_dim, act_dim, net_configs)
-		# self.log_std = nn.Parameter(T.zeros(1, act_dim))
-		# self.log_std = nn.Parameter(-0.5 * T.ones(act_dim, dtype=T.float32))
-		# self.apply(init_weights_)
-
-		# self.mean = nn.Sequential(
-		#     layer_init(nn.Linear(obs_dim, 64)),
-		#     nn.Tanh(),
-		#     layer_init(nn.Linear(64, 64)),
-		#     nn.Tanh(),
-		#     layer_init(nn.Linear(64, act_dim), std=0.01),
-		# )
-		# self.log_std = nn.Parameter(T.zeros(1, act_dim))
+		hid = 256
 
 		self.mean = nn.Sequential(
 		    layer_init(nn.Linear(obs_dim, hid)),
@@ -69,9 +52,7 @@ class PPOPolicy(nn.Module):
 		    layer_init(nn.Linear(hid, act_dim), std=0.01), # PPO-E: Major improvemet!
 			nn.Identity()
 		)
-		# self.log_std = nn.Parameter(T.ones(act_dim, dtype=T.float32))
 		self.log_std = nn.Parameter(-0.5 * T.ones(act_dim, dtype=T.float32), requires_grad=False)
-		# self.log_std = nn.Parameter(T.zeros(act_dim, dtype=T.float32), requires_grad=True)
 
 
 
@@ -100,6 +81,7 @@ class PPOPolicy(nn.Module):
 
 
 	def forward(self, obs, act=None,
+                on_policy=True,
 				reparameterize=False, # Default: True
 				deterministic=False, # Default: False
 				return_log_pi=True, # Default: False
@@ -224,11 +206,20 @@ class StochasticPolicy(nn.Module):
 		lr = net_configs['lr']
 
 		# My suggestions:
-		self.mean_and_log_std_net = MLPNet(obs_dim, 0, net_configs)
-		self.mean = nn.Linear(net_arch[-1], act_dim) # Last layer of Actoe mean
-		self.log_std = nn.Linear(net_arch[-1], act_dim) # Last layer of Actor std
+		# self.mean_and_log_std_net = MLPNet(obs_dim, 0, net_configs)
+		# self.mean = nn.Linear(net_arch[-1], act_dim) # Last layer of Actoe mean
+		# self.log_std = nn.Linear(net_arch[-1], act_dim) # Last layer of Actor std
 
-		self.apply(init_weights_)
+		self.mean_and_log_std_net = nn.Sequential(
+		    layer_init(nn.Linear(obs_dim, net_arch[0])),
+		    nn.Tanh(),
+		    layer_init(nn.Linear(net_arch[0], net_arch[1])),
+		    nn.Tanh()
+		)
+		self.mean = layer_init(nn.Linear(net_arch[-1], act_dim), std=0.01)
+		self.log_std = layer_init(nn.Linear(net_arch[-1], act_dim), std=0.01)
+
+		# self.apply(init_weights_)
 
 		self.obs_bias   = T.zeros(obs_dim)
 		self.obs_scale  = T.ones(obs_dim)
@@ -271,8 +262,8 @@ class StochasticPolicy(nn.Module):
 		return prob, log_prob, entropy
 
 
-	def forward(self,
-				obs, act=None,
+	def forward(self, obs, act=None,
+			    on_policy=True,
 				reparameterize=True, # Default: True
 				deterministic=False, # Default: False
 				return_log_pi=False, # Default: False
@@ -323,16 +314,101 @@ class OVOQPolicy(nn.Module):
 		net_arch = net_configs['arch']
 		optimizer = 'T.optim.' + net_configs['optimizer']
 		lr = net_configs['lr']
-		pass
+
+		self.mean_and_log_std_net = nn.Sequential(
+		    layer_init(nn.Linear(obs_dim, net_arch[0])),
+		    nn.Tanh(),
+		    layer_init(nn.Linear(net_arch[0], net_arch[1])),
+		    nn.Tanh()
+		)
+		self.mean = layer_init(nn.Linear(net_arch[-1], act_dim), std=0.01)
+		self.log_std_q = layer_init(nn.Linear(net_arch[-1], act_dim), std=0.01)
+		self.log_std_v = nn.Parameter(-0.5 * T.ones(act_dim, dtype=T.float32), requires_grad=False)
+
+		self.obs_bias   = T.zeros(obs_dim)
+		self.obs_scale  = T.ones(obs_dim)
+
+		self.act_dim = act_dim
+		self.act_bias =  T.FloatTensor( (act_up_lim + act_low_lim) / 2.0 )#.to(device)
+		self.act_scale = T.FloatTensor( (act_up_lim - act_low_lim) / 2.0 )#.to(device)
+
+		self.to(device)
+
+		self.optimizer = eval(optimizer)(self.parameters(), lr)
 
 
 	def forward(self, obs, act=None,
+				on_policy=True,
 				reparameterize=True, # Default: True
 				deterministic=False, # Default: False
 				return_log_pi=False, # Default: False
 				return_entropy=True
 				):
-		pass
+
+		if isinstance(obs, T.Tensor):
+			obs = (obs.to(self.device) - self.obs_bias) / (self.obs_scale + epsilon)
+		else:
+			obs = (obs - self.obs_bias.cpu().numpy()) / (self.obs_scale.cpu().numpy() + epsilon)
+
+		mean, std = self.pi_mean_std(obs, on_policy)
+
+		log_pi, entropy = None, None
+
+		if deterministic: # Evaluation
+			with T.no_grad():
+				pi = mean if on_policy else T.tanh(mean)
+		else: # Stochastic | Interaction | Policy Evaluation/Improvement
+			pi, log_pi, entropy = self.pi_prob(act, mean, std, on_policy, reparameterize, return_log_pi, return_entropy=True)
+
+		pi = (pi * self.act_scale) + self.act_bias
+
+		return pi, log_pi, entropy
+
+
+	def pi_mean_std(self, obs, on_policy=True):
+		obs = T.as_tensor(obs, dtype=T.float32).to(self.device)
+
+		net_out = self.mean_and_log_std_net(obs)
+		mean = self.mean(net_out)
+
+		if on_policy:
+			log_std = self.log_std_v
+		else:
+			log_std = self.log_std_q(net_out)
+
+		log_std = T.clamp(log_std, min=LOG_STD_MIN, max=LOG_STD_MAX)
+		std = T.exp(log_std)
+		return mean, std
+
+
+	def pi_prob(self, act, mean, std, on_policy, reparameterize, return_log_prob, return_entropy):
+
+		normal_ditribution = Normal(mean, std)
+
+		if reparameterize:
+			sample = normal_ditribution.rsample()
+		else:
+			sample = normal_ditribution.sample()
+
+		if on_policy:
+			prob = sample
+		else:
+			prob = T.tanh(sample)
+
+		log_prob, entropy = None, None
+
+		if return_log_prob:
+			if on_policy:
+				if act is None: act = sample
+				log_prob = normal_ditribution.log_prob(act).sum(axis=-1, keepdims=True)
+			else:
+				log_prob = normal_ditribution.log_prob(sample)
+				log_prob -= T.log( self.act_scale * (1 - prob.pow(2)) + epsilon )
+				log_prob = log_prob.sum(-1, keepdim=True)
+		if return_entropy:
+			entropy = normal_ditribution.entropy().sum(-1, keepdims=True)
+
+		return prob, log_prob, entropy
 
 
 	def to(self, device):
@@ -340,4 +416,4 @@ class OVOQPolicy(nn.Module):
 		self.obs_scale = self.obs_scale.to(device)
 		self.act_bias = self.act_bias.to(device)
 		self.act_scale = self.act_scale.to(device)
-		return super(StochasticPolicy, self).to(device)
+		return super(OVOQPolicy, self).to(device)
