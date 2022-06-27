688e3c96c09f125c2f30e75c6e28290c32f24494
RamiSketcher <ramisketcher@gmail.com>
2022-06-27 07:49:13 +0200

    Update algs/cfgs

--- rl/configs/mbpo_hopper.py
+++ rl/configs/mbpo_hopper.py
@@ -23,7 +23,7 @@ configurations = {
 
             'env_steps' : 1, # E: interact E times then train
             'grad_WM_steps': 0, # G: ac grad
-            'grad_SAC_steps': 20, #20, # ACG: ac grad, 40
+            'grad_SAC_steps': 25, #20, # ACG: ac grad, 40
 
             'policy_update_interval': 1,
             'alpha_update_interval': 1,
@@ -126,7 +126,8 @@ configurations = {
         'optimize_memory_usage': False,
         'buffer_size': int(5e5),
         'model_buffer_size': int(1e7),
-        'real_ratio': 0.05,
+        'real_ratio': 0.04,
+        # 'real_ratio': 0.05,
         # 'real_ratio': 0.0,
         'model_val_ratio': 0.2,
         'oq_rollout_batch_size': 1e5,
