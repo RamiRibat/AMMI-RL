3ce124f788e12b982de3a3b3f783c99ca5614501
RamiSketcher <ramisketcher@gmail.com>
2022-06-28 15:08:33 +0200

    Updates

--- rl/configs/mbppo_hopper.py
+++ rl/configs/mbppo_hopper.py
@@ -2,41 +2,36 @@
 
 configurations = {
 
-    'Comments': {
-        'Rami': 'This was a nightmare!'
-    },
-
     'environment': {
             'name': 'Hopper-v2',
             'type': 'gym-mujoco',
             'state_space': 'continuous',
             'action_space': 'continuous',
             'horizon': 1e3,
-            'action_repeat': 1, # New (leave this)
         },
 
     'algorithm': {
-        'name': 'MBPPO',
-        'mode': 'PAL', # P: cons (few PG steps) | M: Aggr (many model updates + small real buffer)
-        # 'mode': 'MAL', # P: Aggr (many PG steps) | M: Cons (few model updates + large real buffer)
+        'name': 'MBPO',
         'model-based': True,
-        'on-policy': True,
+        'on-policy': False,
         'learning': {
-            'epochs': 200, # N epochs
+            'epochs': 1000, # N epochs
             'epoch_steps': 1000, # NT steps/epoch
-            'init_epochs': 2, # Ni-- PAL: 5 | MAL: 10
-            'expl_epochs': 2, # Nx-- PAL: 5 | MAL: 10
+            'init_epochs': 5, # Ni epochs = 5000 exploration steps
+            'expl_epochs': 10, # Nx epochs
+            'real_epochs': 0, # Nr epochs
 
-            'env_steps' : 1000, # E: interact E times then train
-            'grad_WM_steps': 25, # G-- PAL: 25 | MAL: 10
-            'grad_AC_steps': 20, # ACG: ac grad, 40
-            'grad_PPO_steps': 100, # ACG: ac grad, 40
+            'env_steps' : 1, # E: interact E times then train
+            'grad_WM_steps': 50, # G: ac grad
+            'grad_SAC_steps': 20, # ACG: ac grad, 40
 
             'policy_update_interval': 1,
             'alpha_update_interval': 1,
             'target_update_interval': 1,
 
+
             'n_episodes_rollout': -1,
+            # 'arch': [64, dict(vf=[256, 256], pi=[128])], # [shared, dict(non-shared)]
 
             'use_sde': False,
             'sde_sample_freq': -1,
@@ -55,24 +50,42 @@ configurations = {
 
     'world_model': {
         'type': 'PE',
-        'num_ensembles': 7,
-        'num_elites': 5,
-        # 'num_ensembles': 4,
-        # 'num_elites': 4,
+        'num_ensembles': 2, # 7
+        'num_elites': 2, # 5
         'sample_type': 'Random',
         'learn_reward': True,
         'model_train_freq': 250,
         'model_retain_epochs': 1,
         'rollout_schedule': [20, 100, 1, 15],
         'network': {
-            # 'arch': [512, 512],
-            'arch': [200, 200, 200, 200],
+            'arch': [200,200,200,200], #@#
+            'init_weights': 3e-3,
+            'init_biases': 0,
+            'activation': 'LeakyReLU',
+            'output_activation': 'nn.Identity',
+            'optimizer': "Adam", #@#
+            'lr': 1e-3, #@#
+            'wd': 1e-5,
+            'dropout': None,
+            'batch_size': 250,
+            'device': "auto",
+        }
+    },
+
+    'actor': {
+        'type': 'gaussianpolicy',
+        'action_noise': None, # Optional
+        'alpha': 0.2, # Temprature/Entropy #@#
+        'automatic_entropy': False, # trainer_kwargs
+        'target_entropy': "auto",
+        'network': {
+            'arch': [256,256], #@#
             'init_weights': 3e-3,
             'init_biases': 0,
             'activation': 'ReLU',
             'output_activation': 'nn.Identity',
             'optimizer': "Adam", #@#
-            'lr': 1e-3, #@#
+            'lr': 3e-4, #@#
             'wd': 1e-5,
             'dropout': None,
             'batch_size': 256,
@@ -80,42 +93,23 @@ configurations = {
         }
     },
 
-    'actor': {
-        'type': 'ppopolicy',
-        'constrained': False,
-        'action_noise': None,
-        'clip_eps': 0.2,
-        'kl_targ': 0.02, # 0.03
-        'max_dev': 0.15,
-        'entropy_coef': 0.0,
-        # 'normz_step_size': 0.01,
-        'network': {
-            # 'arch': [128, 128],
-            # 'activation': 'Tanh',
-            'arch': [256, 128, 64],
-            'activation': 'ReLU',
-            'output_activation': 'nn.Identity',
-            'optimizer': "Adam",
-            'lr': 3e-4,
-            'max_grad_norm': 0.5,
-        }
-    },
-
     'critic': {
-        'type': 'V',
-        'number': 1,
-        'gamma': 0.995, # Discount factor - γ
-        'lam': 0.99, # GAE - λ
+        'type': 'sofQ',
+        'number': 2,
+        'gamma': 0.99,
+        'tau': 5e-3,
         'network': {
-            # 'arch': [128, 128],
-            # 'activation': 'Tanh',
-            'arch': [256, 128, 64],
+            'arch': [256,256], #@#
+            'init_weights': 3e-3,
+            'init_biases': 0,
             'activation': 'ReLU',
             'output_activation': 'nn.Identity',
-            'optimizer': "Adam",
-            # 'lr': 1e-3,
-            'lr': 3e-4,
-            'max_grad_norm': 0.5,
+            'optimizer': "Adam", #@#
+            'lr': 3e-4, #@#
+            'wd': 1e-5,
+            'dropout': None,
+            'batch_size': 256,
+            'device': "auto",
         }
     },
 
@@ -123,14 +117,20 @@ configurations = {
     'data': {
         'buffer_type': 'simple',
         'optimize_memory_usage': False,
-        'buffer_size': int(1e4), # PAL: small- 1e4 | MAL: large- 1e5
-        'ov_model_buffer_size': int(1e4),
+        'buffer_size': int(5e5),
+        'model_buffer_size': int(1e7),
+        'real_ratio': 0.05,
+        'model_val_ratio': 0.2,
+        'rollout_batch_size': 400,
+        'model_batch_size': 256,
+        'batch_size': 256,
         'device': "auto",
     },
 
 
     'experiment': {
         'verbose': 0,
+        'WandB': True,
         'print_logs': True,
     }
 }
