7126b1db697a1b93adb981a9c11b54384f249a0a
RamiSketcher <ramisketcher@gmail.com>
2022-07-10 16:02:18 +0200

    Updates

--- rl/algorithms/mbrl/mbpo.py
+++ rl/algorithms/mbrl/mbpo.py
@@ -534,7 +534,7 @@ def main(exp_prefix, config, seed, device, wb):
     wm_epochs = configs['algorithm']['learning']['grad_WM_steps']
     DE = configs['world_model']['num_ensembles']
 
-    group_name = f"{env_name}-{alg_name}-Exp-01"
+    group_name = f"{env_name}-{alg_name}-Exp-A"
     # group_name = f"{env_name}-{alg_name}-GCP-0"
     exp_prefix = f"seed:{seed}"
 
