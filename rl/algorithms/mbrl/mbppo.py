541ffea56fe51d74f40eb3d14f4ddc234095215e
RamiSketcher <ramisketcher@gmail.com>
2022-07-31 18:12:11 +0300

    Update algs/cfgs

--- rl/algorithms/mbrl/mbppo.py
+++ rl/algorithms/mbrl/mbppo.py
@@ -827,7 +827,7 @@ def main(exp_prefix, config, seed, device, wb):
     wm_epochs = configs['algorithm']['learning']['grad_WM_steps']
     DE = configs['world_model']['num_ensembles']
 
-    group_name = f"{env_name}-{alg_name}-Tanh(Pi)-V2-17" # Local
+    group_name = f"{env_name}-{alg_name}-Tanh(Pi)-V2-18" # Local
     # group_name = f"{env_name}-{alg_name}-GCP-0" # GCP
     exp_prefix = f"seed:{seed}"
 
