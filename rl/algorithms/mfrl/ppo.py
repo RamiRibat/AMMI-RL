d184b94e7141a09e4e9e8d0dace1a5a08f5178d4
RamiSketcher <ramisketcher@gmail.com>
2022-08-01 10:17:17 +0300

    Update mbppo

--- rl/algorithms/mfrl/ppo.py
+++ rl/algorithms/mfrl/ppo.py
@@ -556,7 +556,7 @@ def main(exp_prefix, config, seed, device, wb):
     env_name = configs['environment']['name']
     env_type = configs['environment']['type']
 
-    group_name = f"{env_name}-{alg_name}-V2-43" # H < -2.7
+    group_name = f"{env_name}-{alg_name}-V2-45" # H < -2.7
     exp_prefix = f"seed:{seed}"
 
     if wb:
