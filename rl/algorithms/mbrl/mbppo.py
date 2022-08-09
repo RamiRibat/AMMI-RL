f876cc42a65e31b0277903d9b6b1ae99e524ce5c
RamiSketcher <ramisketcher@gmail.com>
2022-08-09 03:51:09 +0300

    Update mbppo-43

--- rl/algorithms/mbrl/mbppo.py
+++ rl/algorithms/mbrl/mbppo.py
@@ -870,7 +870,7 @@ def main(exp_prefix, config, seed, device, wb):
 	wm_epochs = configs['algorithm']['learning']['grad_WM_steps']
 	DE = configs['world_model']['num_ensembles']
 
-	group_name = f"{env_name}-{alg_name}-42" # Local
+	group_name = f"{env_name}-{alg_name}-43" # Local
 	# group_name = f"{env_name}-{alg_name}-GCP-0" # GCP
 	exp_prefix = f"seed:{seed}"
 
